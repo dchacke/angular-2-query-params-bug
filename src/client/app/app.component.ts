0c047c7e4bd52bc5bc06fb006c628b55b8fd0bad
Dennis Charles Hackethal <dennis.hackethal@gmail.com>
2016-10-10 11:00:41 -0700

    Reproduce problem with query params

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Config } from './shared/index';
 import './operators';
 
+import { ActivatedRoute } from '@angular/router';
+
 /**
  * This class represents the main application component. Within the @Routes annotation is the configuration of the
  * applications routes, configuring the paths for the lazy loaded components (HomeComponent, AboutComponent).
@@ -13,7 +15,15 @@ import './operators';
 })
 
 export class AppComponent {
-  constructor() {
+  constructor(private route: ActivatedRoute) {
     console.log('Environment config', Config);
+  }
+
+  ngOnInit() {
+    // This will fire with an empty object,
+    // even when the page is opened with query params
+    this.route.queryParams.subscribe(params => {
+      console.log('Query params are', params)
+    });
   }
 }
